b5ec4d801b54d3a526382d238ff3553cffee3c5f
Chinecherem Harry <chinecherempharry@gmail.com>
2023-12-26 03:30:47 +0100

    modified innovatum experience date

--- components/works/Innovatum.tsx
+++ components/works/Innovatum.tsx
@@ -4,7 +4,7 @@ const Innovatum = () => {
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.1 }} className="w-full">
             <h3 className="flex gap-1 font-medium text-xl font-titleFont" >Frontend Developer Intern<span className="text-textGreen tracking-wide" >@Innovatum AB</span> </h3>
-            <p className="text-sm mt-1 font-medium text-textDark" >Jan 2022 - May 2022</p>
+            <p className="text-sm mt-1 font-medium text-textDark" >November 2019 - March 2020</p>
         </motion.div>
     )
 }
