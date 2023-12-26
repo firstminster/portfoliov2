654542462c9b9a459fec25a8c1978fe0e06c1dc7
Chinecherem Harry <chinecherempharry@gmail.com>
2023-12-26 03:26:00 +0100

    modified the contact message and the email height

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -5,7 +5,7 @@ const RightSide = () => {
         <div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textLight font-titleFont'>
 
             <a href="mailto:promiseharrycyber@gmail.com">
-                <p className='w-auto h-72 text-sm rotate-90 tracking-widest text-textGreen  inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>promiseharrycyber@gmail.com</p>
+                <p className='w-auto h-64 text-sm rotate-90 tracking-widest text-textGreen  inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300'>promiseharrycyber@gmail.com</p>
             </a>
 
             <span className="w-[2px] h-32 bg-textDark inline-flex"></span>
